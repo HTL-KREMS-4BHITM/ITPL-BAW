772daaddfeb7d0bcc679b704ecbd6dcfa40f1c17
Raphael <raphael.nigl@outlook.de>
2024-11-16 15:12:32 +0100

    [TASK] Rider changed some using stuff can be savely ignored

--- BAWLib/obj/Debug/net8.0/BAWLib.AssemblyInfo.cs
+++ BAWLib/obj/Debug/net8.0/BAWLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BAWLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1563122609b83e7688bc261b1bba09762bb8b5b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f236089c2ff6e1d2ba84232d973cbb9f76d8ec6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BAWLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BAWLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
