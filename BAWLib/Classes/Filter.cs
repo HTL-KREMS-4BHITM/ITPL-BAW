0aba9d4eb0ef1a3b648d62c44158456a98ba77d4
werwolfy <raphael.nigl@outlook.de>
2025-01-19 14:02:37 +0100

    [TASK] Filter by date Workes

--- BAWLib/Classes/Filter.cs
+++ BAWLib/Classes/Filter.cs
@@ -4,8 +4,8 @@ public class Filter
 {
     public List<string> Regions { get; set; } = new List<string>();
     public List<string> Brands { get; set; } = new List<string>();
-    public int HorsePower { get; set; }
-    public int Price { get; set; }
-    public DateOnly From { get; set; }
-    public DateOnly To { get; set; }
+    public int HorsePower { get; set; } = 300;
+    public int Price { get; set; } = 0;
+    public DateOnly From { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    public DateOnly To { get; set; }= DateOnly.FromDateTime(DateTime.Now).AddYears(1);
 }
