aa6eafc84987deaec014e6c8a8a384b5bdb3b5d7
werwolfy <raphael.nigl@outlook.de>
2025-01-23 13:20:44 +0100

    [TASK] Worked on favorite icon

--- BAWLib/Entities/Favorite.cs
+++ BAWLib/Entities/Favorite.cs
@@ -9,4 +9,22 @@ public class Favorite
     public int User_ID { get; set; }
     public Motorbike Motorbike { get; set; }
     public int Motorbike_ID { get; set; }
+
+    protected bool Equals(Favorite other)
+    {
+        return User_ID == other.User_ID && Motorbike_ID == other.Motorbike_ID;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Favorite)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(User_ID, Motorbike_ID);
+    }
 }
